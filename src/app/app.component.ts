e6f4e4dea38fa38baeffd5900b8bf63e9ecdaf99
Matt Callahan <matthew.callahan@outlook.com>
2020-12-18 13:19:28 -0700

    css => scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import config from '@arcgis/core/config.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   private view: any = null;
